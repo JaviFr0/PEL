1d0e276ce54d7f2faf5657d9d5916d153be0d61a
DESKTOP-Q398LVG\kikor <javierfr999888@gmail.com>
2023-01-22 18:38:39 +0100

    2

--- untitled/cmake-build-debug/cmake_install.cmake
+++ untitled/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Javier/3D Objects/Proyecto/untitled
+# Install script for directory: C:/Users/kikor/Music/PEL/PEL/untitled
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.3/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/kikor/AppData/Local/JetBrains/CLion 2022.3/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Javier/3D Objects/Proyecto/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/kikor/Music/PEL/PEL/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,6 +1,66 @@
-#include <iostream>
+/* En el palacio de Buckingham se ha colado el malo de Skyfall y ha borrado los datos de
+la tesoreria de la reina. El hijo, dosgustado, ha pedido a su mejor
+agente que recupere los datos. Para ello, 007 ha contactado con nosotros para que preparemos
+un software que recupere los datos. Este software debe almacenar
+lso datos de la base del malo en estructuras enlazadas de tipo FIFO. James Bond recuperara esos
+datos y lso alamacenara en estructuras estáticas manteniendo
+el tamaño original de la estructura enlazada. Una vez recuperado este dato se devolvera a la
+estructura de al reina, Para introducirlos en el servidor de la
+debera usarse el metodo pushback. Una vez recuperados los datos y devueltos al servidor,
+007 tendra qu hacer una busqueda en dicha estructura de forma
+que recupere la informacion de la direccion del sobrino de la reina. La estructra de estos datos
+ son el nombre de la persona, direccion y un identificador
+del dato. Como medida restrictiva y para vuestro proximo proyecto teneis prohibido el uso de bucles.
+ (no es obligatorio lo de los bucles)
+*/
+#include <queue>
+#include <vector>
+#include <string>
+ using namespace std;
+// estructura para un nodo de la cola enlazada
+struct Node {
+    std::string name;
+    std::string address;
+    int id;
+    Node* next;
+};
+
+// función para pasar los datos de la cola enlazada a la cola estática
+void transferData(Node* front, std::vector<Node>& staticQueue) {
+    Node* curr = front;
+    while (curr != nullptr) {
+        // agrega el elemento a la cola estática
+        staticQueue.push_back(*curr);
+        // avanza al siguiente elemento
+        curr = curr->next;
+    }
+}
+
+// función para buscar información en la estructura
+std::string search(std::vector<Node>& staticQueue, std::string name) {
+    for (auto& node : staticQueue) {
+        if (node.name == name) {
+            return node.address;
+        }
+    }
+    return "No se encontró la información buscada";
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // ejemplo de cola enlazada
+    Node n1 = {"Alice", "123 Main St", 1};
+    Node n2 = {"Bob", "456 Park Ave", 2};
+    Node n3 = {"Charlie", "789 Elm St", 3};
+    n1.next = &n2;
+    n2.next = &n3;
+
+    std::vector<Node> staticQueue;
+    // llama a la función para pasar los datos de la cola enlazada a la cola estática
+    transferData(&n1, staticQueue);
+
+    // llama a la función para buscar información en la estructura
+    std::cout << "Dirección de Bob: " << search(staticQueue, "Bob") << std::endl;
+
     return 0;
 }
+
